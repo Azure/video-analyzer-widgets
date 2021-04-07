5d263c46fbc2f0fe902c60d5488f2e13fa02056c
Moris <moriarais@gmail.com>
2021-04-07 15:22:55 +0300

    Fix example.html

--- packages/web-components/src/line-drawer/line-drawer.spec.ts
+++ packages/web-components/src/line-drawer/line-drawer.spec.ts
@@ -1,24 +1,24 @@
 import { html, fixture, expect } from '@open-wc/testing';
 
-import { LineDrawerComponent } from './line-drawer-component';
+import { LineDrawerComponent } from './line-drawer.component';
 
 LineDrawerComponent;
 
 describe('LineDrawerComponent', () => {
     it('has a default text "this is example component"', async () => {
-        const el = await fixture<LineDrawerComponent>(html`<line-drawer-component></line-drawer-component>`);
+        const el = await fixture<LineDrawerComponent>(html`<line-drawer></line-drawer>`);
 
         expect(el.borderColor).to.equal('this is example component');
     });
 
     it('can override the text via attribute', async () => {
-        const el = await fixture<LineDrawerComponent>(html`<line-drawer-component text="Hello"></line-drawer-component>`);
+        const el = await fixture<LineDrawerComponent>(html`<line-drawer text="Hello"></line-drawer>`);
 
         expect(el).to.equal('Hello');
     });
 
     it('passes the a11y audit', async () => {
-        const el = await fixture<LineDrawerComponent>(html`<line-drawer-component></line-drawer-component>`);
+        const el = await fixture<LineDrawerComponent>(html`<line-drawer></line-drawer>`);
 
         await expect(el).shadowDom.to.be.accessible();
     });
