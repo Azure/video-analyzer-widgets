425968e07e13295618bd903e525231113f12a39e
Nofar Edan <noedan@microsoft.com>
2021-04-08 17:09:30 +0300

    fix build

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,61 +9,51 @@ module.exports = {
         builder: 'webpack5'
     },
     stories: ['../packages/web-components/src/**/*.stories.@(js|jsx|ts|tsx)', '../stories/**/*.stories.mdx'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls', '@storybook/addon-postcss']
-    // webpackFinal: async (config) => {
-    //     config.plugins.push(
-    //         new CircularDependencyPlugin({
-    //             exclude: /node_modules/,
-    //             failOnError: process.env.NODE_ENV === 'production'
-    //         }),
-    //         new HtmlWebpackPlugin()
-    //     );
-    //     return {
-    //         ...config,
-    //         module: {
-    //             ...config.module,
-    //             rules: custom.module.rules
-    //         }
-    //     };
-    //     config.module.rules.push(
-    //         {
-    //             test: /\.ts$/,
-    //             use: [
-    //                 {
-    //                     loader: require.resolve('ts-loader')
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(css|scss)$/,
-    //             use: ['style-loader', 'css-loader', 'sass-loader'],
-    //             include: path.resolve(__dirname, '../packages/styles/')
-    //         },
-    //         {
-    //             test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-    //             use: [
-    //                 {
-    //                     loader: require.resolve('url-loader')
-    //                 }
-    //             ],
-    //             include: path.resolve(__dirname, '../packages/styles/')
-    //         }
-    //         // {
-    //         //     test: /\.scss$/,
-    //         //     use: ['style-loader', 'css-loader', 'sass-loader'],
-    //         //     include: path.resolve(__dirname, '../')
-    //         // }
-    //     );
-    //     config.resolve.extensions.push('.ts');
-    //     config.resolve.extensions.push('.js');
-    //     config.plugins.push(
-    //         new CircularDependencyPlugin({
-    //             exclude: /node_modules/,
-    //             failOnError: process.env.NODE_ENV === 'production'
-    //         }),
-    //         new HtmlWebpackPlugin()
-    //     );
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-controls'],
+    webpackFinal: async (config) => {
+        config.module.rules.push(
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: require.resolve('ts-loader')
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: require.resolve('style-loader')
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: require.resolve('css-loader')
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: require.resolve('sass-loader')
+                    }
+                ]
+            }
+        );
+        config.resolve.extensions.push('.ts');
+        config.resolve.extensions.push('.js');
+        config.plugins.push(
+            new CircularDependencyPlugin({
+                exclude: /node_modules/,
+                failOnError: process.env.NODE_ENV === 'production'
+            })
+        );
 
-    //     return config;
-    // }
+        return config;
+    }
 };
