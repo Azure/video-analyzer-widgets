885ad6ada64406fc02335242a9918957c0da79f4
Moris <moriarais@gmail.com>
2021-04-20 13:34:40 +0300

    remove code

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -4,6 +4,3 @@ export * from './date-picker';
 export * from './segments-timeline';
 export * from './time-ruler';
 export * from './timeline';
-export * from './actions-menu';
-export * from './layer-label';
-export * from './areas-view';
