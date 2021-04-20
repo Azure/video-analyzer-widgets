4277ddfa8f59516aaed9d0ea3678120358f8f0cc
Moris <moriarais@gmail.com>
2021-04-20 14:53:37 +0300

    fix import

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -1,14 +1,6 @@
 export * from './example-web-component';
-<<<<<<< HEAD
-export * from './date-picker';
-=======
 export * from './line-drawer';
->>>>>>> feature/morai/draw-polygon
 export * from './segments-timeline';
 export * from './time-ruler';
 export * from './timeline';
-export * from './editable-text-field';
-export * from './actions-menu';
-export * from './layer-label';
-export * from './areas-view';
 export * from './polygon-drawer';
