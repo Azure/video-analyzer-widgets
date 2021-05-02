fbcff10c17181f0aeeb04fb725d3942bcd3d032a
Ben Bakhar <bebakhar@microsoft.com>
2021-05-02 14:32:33 +0300

    fix(webpack): expose public ams as commonjs modules

--- npm.export.ts
+++ npm.export.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-
 export * from './packages/widgets/src';
 export * from './packages/web-components/src';
 export * from './packages/styles';
