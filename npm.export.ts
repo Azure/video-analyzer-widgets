01c1b47b5f8736a9db376ba871b2a32eb1ae96fc
Doron Mazor <domazor@microsoft.com>
2021-05-12 17:12:47 +0000

    Merged PR 36: fix: timeline event listeners
    fix: timeline event listeners

--- npm.export.ts
+++ npm.export.ts
@@ -3,8 +3,9 @@ export * from './packages/widgets/src';
 export * from './packages/web-components/src';
 export * from './packages/styles';
 
-import { FASTButton, FASTMenu, FASTMenuItem } from '@microsoft/fast-components';
+import { FASTButton, FASTMenu, FASTMenuItem, FASTSlider } from '@microsoft/fast-components';
 
 FASTButton;
 FASTMenu;
 FASTMenuItem;
+FASTSlider;
