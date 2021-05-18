5e2f64dd416dc0f53df6b9ee33de1e1fc3b07908
Nofar Edan <noedan@microsoft.com>
2021-05-18 19:39:09 +0300

    fix after cr

--- global.export.ts
+++ global.export.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { ZoneDrawerWidget } from './packages/widgets/src';
 import { Player } from './packages/widgets/src/rvx/rvx-widget';
-import { FASTButton, FASTMenu, FASTMenuItem } from '@microsoft/fast-components';
+import { FASTButton, FASTMenu, FASTMenuItem, FASTSlider } from '@microsoft/fast-components';
 
 Player;
 FASTButton;
 FASTMenu;
 FASTMenuItem;
 ZoneDrawerWidget;
+FASTSlider;
 
 export const widgets = {
     player: Player,
