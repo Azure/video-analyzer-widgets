25f055d41adf8097a105656f8638de1d31eb3c0e
Ben Bakhar <bebakhar@microsoft.com>
2021-05-23 14:34:37 +0300

    fix(build): remove zone drawer from build

--- npm.export.ts
+++ npm.export.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 export * from './packages/widgets/src';
-export * from './packages/web-components/src';
 export * from './packages/styles';
 
 import { FASTButton, FASTMenu, FASTMenuItem, FASTSlider } from '@microsoft/fast-components';
