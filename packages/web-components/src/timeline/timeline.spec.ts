5a76b50785c93280f9152ca7d0042be97f6055f7
Nofar Edan <noedan@microsoft.com>
2021-06-21 19:54:08 +0300

    fix timeline test

--- packages/web-components/src/timeline/timeline.spec.ts
+++ packages/web-components/src/timeline/timeline.spec.ts
@@ -31,11 +31,7 @@ describe('TimelineComponent', () => {
         const el = await fixture<TimelineComponent>(html`<media-timeline></media-timeline>`);
         el.config = config;
         el.configChanged();
-        const isFirefoxBrowser = navigator.userAgent.includes('Firefox');
-        // styles count as element on FF
-        const childLength = isFirefoxBrowser ? 4 : 2;
-
-        await expect(el.shadowRoot.children.length).to.equal(childLength);
+        await expect(el.shadowRoot.children.length).to.equal(4);
     });
 
     xit('with zoom - should have 4 children', async () => {
