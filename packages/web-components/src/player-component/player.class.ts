f2a127520b566bbfe848acd48a3bac2a37ea6fa0
Andy <andzhan@microsoft.com>
2021-10-29 11:49:03 -0700

    Address comments

--- packages/web-components/src/player-component/player.class.ts
+++ packages/web-components/src/player-component/player.class.ts
@@ -668,20 +668,24 @@ export class PlayerWrapper {
         const MAX_LATENCY_WINDOW = 3; // in seconds
         this._driftCorrectionTimer = window.setInterval(async () => {
             const video = this.player.getMediaElement() as HTMLMediaElement;
-            const buffer = this.player.getBufferedInfo().video[0];
-            if (!video.paused && this.isLive && !this.player.isBuffering() && buffer &&
-                buffer.end - MAX_LATENCY_WINDOW > video.currentTime
-            ) {
+
+            if (video && !video.paused && this.isLive && !this.player.isBuffering()) {
+                const videoBuffered = this.player.getBufferedInfo().video;
+                const videoCurrentTime = video.currentTime; // Lock in the current value
+                const videoBufferedEnd = videoBuffered.length > 0 ? videoBuffered[videoBuffered.length - 1].end : videoCurrentTime - 1;
+
+                if (videoBufferedEnd - MAX_LATENCY_WINDOW > videoCurrentTime) {
                 // TODO: To account for the time that elapses during the trickplay portion, we need to add some additional trickplay duration
                 // in order to get latency as low as possible. The slower the trickplay speed, and the higher the latency, the higher this
                 // additional value will have to be. For now we use a flat value.
-                const additionalDur= 2000; // in milliseconds
-                const delta = buffer.end - video.currentTime;
-                Logger.log(`Correcting drift, jumping forward ${delta}`);
-                video.playbackRate=2;
-                await new Promise(r => setTimeout(r, (delta*1000/video.playbackRate + additionalDur)));
-                Logger.log('Resetting to 1x playback rate...');
-                video.playbackRate=1;
+                    const additionalDur= 2000; // in milliseconds
+                    const delta = videoBufferedEnd - video.currentTime;
+                    Logger.log(`Correcting drift, jumping forward ${delta}`);
+                    video.playbackRate=2;
+                    await new Promise(r => setTimeout(r, (delta*1000/video.playbackRate + additionalDur)));
+                    Logger.log('Resetting to 1x playback rate...');
+                    video.playbackRate=1;
+                }
             }
         }, driftIntervalMs);
         // Add bounding box drawer
