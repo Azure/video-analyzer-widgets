ec5691a05856fa70714efc9742ab72484949f8ca
sxian0613@gmail.com <sxian0613@gmail.com>
2021-12-29 21:26:06 -0800

    group-similar-rules-together

--- packages/web-components/src/player-component/player-component.style.ts
+++ packages/web-components/src/player-component/player-component.style.ts
@@ -286,18 +286,12 @@ export const styles = css`
         max-height: calc(100% - ${timelineHeight});
     }
 
-    :host(.live-off.timeline-off.bottom-controls-on) .fullscreen video {
-        max-height: calc(100% - ${bottomControlHeight});
-    }
-
-    :host(.live-off.timeline-off.bottom-controls-off) .fullscreen video {
-        max-height: calc(100%);
-    }
-
+    :host(.live-off.timeline-off.bottom-controls-on) .fullscreen video,
     :host(.live-on.bottom-controls-on) .fullscreen video {
         max-height: calc(100% - ${bottomControlHeight});
     }
 
+    :host(.live-off.timeline-off.bottom-controls-off) .fullscreen video,
     :host(.live-on.bottom-controls-off) .fullscreen video {
         max-height: calc(100%);
     }
